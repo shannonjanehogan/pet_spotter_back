d50b6ab232cc08189c8dba75616d688aa2767881
shannonjanehogan <shannon.jane.hogan@gmail.com>
2019-03-10 14:45:56 -0700

    Create insertion values for Breeds table

--- database/migrations/create_shelter.sql
+++ database/migrations/create_shelter.sql
@@ -1,12 +1,11 @@
 CREATE TABLE Shelter (
   SName char(30),
   SEmail char(50),
-  SPhone char (11),
+  SPhone char (11) PRIMARY KEY,
   Capacity int,
   SHouseNo int               NOT NULL,
   SStreet char(100)          NOT NULL,
   SPostalCode char(7)        NOT NULL,
-  PRIMARY KEY (SPhone),
   UNIQUE (SHouseNo, SStreet, SPostalCode),
   FOREIGN KEY (SHouseNo, SStreet, SPostalCode) references Address (HouseNo, Street, PostalCode)
 );
---
+++ database/seeds/insert_breeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO Breed VALUES ('Border Collie', 'Dog', 0, 1, 1);
+INSERT INTO Breed VALUES ('Golden Retriever', 'Dog', 1, 1, 1);
+INSERT INTO Breed VALUES ('Shiba Inu', 'Dog', 0, 0, 0);
+INSERT INTO Breed VALUES ('Bengal', 'Cat', 1, 1, 0);
+INSERT INTO Breed VALUES ('Maine Coon', 'Cat', 1, 1, 1);
+INSERT INTO Breed VALUES ('Siamese', 'Cat', 1, 0, 1);
