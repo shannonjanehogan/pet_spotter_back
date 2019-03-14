2f77a25b318f39321ff815aceb21777c738af907
shannonjanehogan <shannon.jane.hogan@gmail.com>
2019-03-14 13:19:38 -0700

    Add body parser for post/put requests

--- server.js
+++ server.js
@@ -1,13 +1,51 @@
 const express = require('express');
-const app = express();
 const port = 3000;
 const database = require('./database/index.js');
+var bodyParser = require('body-parser')
+const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/test', (req, res) => res.send('Hello World!'));
 
+// View All Animals
 app.get('/animals', async (req, res) => {
   const result = await database.selectQuery('select_animals');
   res.send(result);
 });
+
+// View All Animals by Shelter
+app.get('/shelters/:id/animals', async (req, res) => {
+  const result = await database.runQuery(
+    `SELECT * FROM Animal INNER JOIN Shelter on Shelter.Sphone = Animal.Sphone
+    WHERE Shelter.Sphone = 6048324820;`
+  );
+  res.send(result.rows);
+});
+
+app.post('/donations', async (req, res) => {
+  try {
+    const result = await database.runQuery(`INSERT INTO Donation
+  (TransactionID, Amount, Date, CPhone, SPhone) VALUES (
+      '2017-0002',
+      ${req.body.amount},
+      '2018-01-22',
+      (SELECT CPhone FROM Client WHERE CPhone = '7884321987'),
+      (SELECT SPhone FROM Shelter WHERE SPhone = '6048324820')
+    );`);
+    res.send(result);
+  } catch {
+    res.sendStatus(500);
+  }
+});
+
+app.put('/client/:id', async (req, res) => {
+  res.sendStatus(200);
+}
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
