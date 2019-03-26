2ebcaebd30056b8ae4f043082b56040a0f9cfe1e
Jordan Chiu <jordanmchiu@gmail.com>
2019-03-26 13:32:32 -0700

    Adding names to credit for each donation that exists

--- database/seeds/insert_name_to_credit.sql
+++ database/seeds/insert_name_to_credit.sql
@@ -23,3 +23,28 @@ INSERT INTO NameToCredit (Name, Description, TransactionID) VALUES (
   'Sending love from Ontario!',
   (SELECT TransactionID FROM Donation WHERE TransactionID = 5)
 );
+INSERT INTO NameToCredit (Name, Description, TransactionID) VALUES (
+  'Anonymous',
+  'Whoo',
+  (SELECT TransactionID FROM Donation WHERE TransactionID = 6)
+);
+INSERT INTO NameToCredit (Name, Description, TransactionID) VALUES (
+  'Anonymous',
+  'Messages are great',
+  (SELECT TransactionID FROM Donation WHERE TransactionID = 7)
+);
+INSERT INTO NameToCredit (Name, Description, TransactionID) VALUES (
+  'Anonymous',
+  'Donations are great',
+  (SELECT TransactionID FROM Donation WHERE TransactionID = 8)
+);
+INSERT INTO NameToCredit (Name, Description, TransactionID) VALUES (
+  'Anonymous',
+  'You all are great',
+  (SELECT TransactionID FROM Donation WHERE TransactionID = 9)
+);
+INSERT INTO NameToCredit (Name, Description, TransactionID) VALUES (
+  'Anonymous',
+  'I love money',
+  (SELECT TransactionID FROM Donation WHERE TransactionID = 10)
+);
