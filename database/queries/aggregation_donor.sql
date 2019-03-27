f701d63832a3dac98e4d7c4c62fead34e2b47178
Jordan Chiu <jordanmchiu@gmail.com>
2019-03-26 18:44:42 -0700

    Added donor name to tax receipt query

--- database/queries/aggregation_donor.sql
+++ database/queries/aggregation_donor.sql
@@ -1,10 +1,10 @@
-SELECT Donor.CPhone, SUM(Amount), COUNT(*)
-FROM Donor, Donation
+SELECT Donor.CPhone, Client.CName, SUM(Amount), COUNT(*)
+FROM Donor, Donation, Client
 WHERE (
+	Donor.CPhone = Client.CPhone AND
     Donor.CPhone = Donation.CPhone AND
     to_char(Donation.date, 'yyyy') = $1
     -- to_char(Donation.date, 'yyyy') = varYear;
 )
-GROUP BY Donor.CPhone
+GROUP BY Donor.CPhone, Client.CName
 HAVING Donor.CPhone = $2;
--- HAVING Donor.CPhone = varPhone
