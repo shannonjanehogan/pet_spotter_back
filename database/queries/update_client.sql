917fb8828a4f5f63cbe18dd0e19aeb17d41a6f49
Jordan Chiu <jordanmchiu@gmail.com>
2019-03-27 11:35:29 -0700

    Fixed multiple rows being returned in subquery

--- database/queries/update_client.sql
+++ database/queries/update_client.sql
@@ -1,8 +1,8 @@
 UPDATE client
 SET
     CName = $1,
-    CHouseNo = (SELECT HouseNo FROM Address WHERE HouseNo = $2),
-    CStreet = (SELECT Street FROM Address WHERE Street = $3),
+    CHouseNo = (SELECT DISTINCT HouseNo FROM Address WHERE HouseNo = $2),
+    CStreet = (SELECT DISTINCT Street FROM Address WHERE Street = $3),
     CPostalCode = (SELECT PostalCode FROM AddressPostalCode WHERE PostalCode = $4),
     CEmail = $5
 WHERE CPhone = $6;
